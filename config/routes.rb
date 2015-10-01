a437217985fc2738812bf43eafc13bbe90a1adeb
rei___yu <satomi.i.sakura@gmail.com>
2015-10-01 18:38:40 +0900

    create comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :comments
   resources :boards
   root to: "home#index"
   # The priority is based upon order of creation: first created -> highest priority.
