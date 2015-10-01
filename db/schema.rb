193570ba05ab5c240a2e8f724b496fe30643b67c
rei___yu <satomi.i.sakura@gmail.com>
2015-10-01 18:54:42 +0900

    change root to boards

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151001084417) do
+ActiveRecord::Schema.define(version: 20151001093636) do
 
   create_table "boards", force: :cascade do |t|
     t.string   "title",      limit: 255
     t.string   "overview",   limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "text",       limit: 255
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
   end
